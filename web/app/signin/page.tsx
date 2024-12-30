6e283637d023f9be981a92ff922f53d67e1c39bd
Cra2yqi <crzqi1022@gmail.com>
2024-12-30 11:27:32 +0800

    修改部分dify信息

--- web/app/signin/page.tsx
+++ web/app/signin/page.tsx
@@ -1,14 +1,13 @@
 'use client'
 import { useSearchParams } from 'next/navigation'
-import OneMoreStep from './oneMoreStep'
 import NormalForm from './normalForm'
 
 const SignIn = () => {
   const searchParams = useSearchParams()
   const step = searchParams.get('step')
 
-  if (step === 'next')
-    return <OneMoreStep />
+  // if (step === 'next')
+  //   return <OneMoreStep />
   return <NormalForm />
 }
 
